92d64ee6518e08229d6b379b2be18fe0df8124bb
aasf0543a <33273330+aasf0543a@users.noreply.github.com>
2017-11-02 17:19:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -278,13 +278,13 @@ def technews():
 
 
 def panx():
-    target_url = 'https://panx.asia/'
+    target_url = 'https://technews.tw/author/cool3c/'#癮科技
     print('Start parsing ptt hot....')
     rs = requests.session()
     res = rs.get(target_url, verify=False)
     soup = BeautifulSoup(res.text, 'html.parser')
     content = ""
-    for data in soup.select('div.container div.row div.desc_wrap h2 a'):
+    for data in soup.select('div.class li a title'):
         title = data.text
         link = data['href']
         content += '{}\n{}\n\n'.format(title, link)
@@ -359,7 +359,7 @@ def handle_message(event):
             event.reply_token,
             TextSendMessage(text=content))
         return 0
-    if event.message.text == "PanX泛科技":
+    if event.message.text == "癮科技":
         content = panx()
         line_bot_api.reply_message(
             event.reply_token,
