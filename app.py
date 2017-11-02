95e2b4a3eb8c6d73522e9ae0d6fcf2819479743b
aasf0543a <33273330+aasf0543a@users.noreply.github.com>
2017-11-02 11:38:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -371,7 +371,7 @@ def handle_message(event):
             template=ButtonsTemplate(
                 title='選擇服務',
                 text='請選擇',
-                thumbnail_image_url='https://i.imgur.com/p7tUiEi.jpg',
+                thumbnail_image_url='https://i.imgur.com/p7tUiEis.jpg',
                 actions=[
                     MessageTemplateAction(
                         label='新聞',
