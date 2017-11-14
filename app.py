95a478db0e467d3b78083f6f0bfcc0620a2030af
aasf0543a <33273330+aasf0543a@users.noreply.github.com>
2017-11-14 17:38:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -318,6 +318,7 @@ def apple_Finance():
 def handle_message(event):
     print("event.reply_token:", event.reply_token)
     print("event.message.text:", event.message.text)
+    print(message)
     if event.message.text == "eyny":
         content = eyny_movie()
         line_bot_api.reply_message(
