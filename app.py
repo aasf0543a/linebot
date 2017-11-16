3c2eb199d11e4b7103e76818b866cfc2e064edc5
aasf0543a <33273330+aasf0543a@users.noreply.github.com>
2017-11-16 17:06:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -328,6 +328,12 @@ def handle_sticker_message(event):
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     print("Handle: reply_token: " + event.reply_token + ", message: " + event.message.text)
+    if event.message.text == "patty":
+        content = "大胖子"
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=content))
+        return 0
     if event.message.text == "eyny":
         content = eyny_movie()
         line_bot_api.reply_message(
