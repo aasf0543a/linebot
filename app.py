85c01c77106a3f2fc9e844a2420196b6aaf22562
aasf0543a <33273330+aasf0543a@users.noreply.github.com>
2017-11-17 15:10:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -315,6 +315,11 @@ def apple_Finance():
 #將收到的訊息為Location
 @handler.add(MessageEvent, message=LocationMessage)
 def handle_location_message(event):
+    title=event.message.title 
+    address=event.message.address
+    latitude=event.message.latitude
+    longitude=event.message.longitude
+    print("title:"= title, "address:"= address, "latitude:"= latitude, "longitude:"= longitude)
 #    line_bot_api.reply_message(
 #        event.reply_token,
 #        LocationSendMessage(
