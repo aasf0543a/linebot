365ca72c2bcef0be785d15227ce58ffee7c05a16
aasf0543a <33273330+aasf0543a@users.noreply.github.com>
2017-11-17 16:27:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -318,7 +318,8 @@ def handle_location_message(event):
     title = event.message.title 
     address = event.message.address
     latitude = str(event.message.latitude)
-    print(" address:" + latitude)
+    longitude = str(event.message.longitude)
+    print(" address:" + address + "latitude:" + latitude + "longitude:" + longitude)
     line_bot_api.reply_message(
         event.reply_token,
         LocationSendMessage(title=event.message.title, address=event.message.address,
