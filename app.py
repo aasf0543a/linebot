afa407004793b459a9cfdd75288884c4e540eab1
aasf0543a <33273330+aasf0543a@users.noreply.github.com>
2017-11-18 17:26:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -326,7 +326,6 @@ def handle_location_message(event):
     key = '216d8f5d9335dc049e264d38ab7d18f9'   # api key
     url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&APPID=' + key   # full url
     parsed_json = json.loads(url)
-    print(parsed_json)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage("OK")
