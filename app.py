6d7efc0b8f1f4a1a188b0ffa4b9b40900a86c610
aasf0543a <33273330+aasf0543a@users.noreply.github.com>
2017-11-18 18:26:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -328,7 +328,7 @@ def handle_location_message(event):
     r = requests.get(url)
     dict = r.json()
     select_data = dict['name']
-        print(select_data)
+    print(select_data)
         line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage("OK")
