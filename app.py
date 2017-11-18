68fc1e109e5459a0a422b528b80f279f1272fcc5
aasf0543a <33273330+aasf0543a@users.noreply.github.com>
2017-11-18 21:04:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -338,8 +338,10 @@ def handle_location_message(event):
     sys_sun = dict['sys']
     sunrise = sys_sun.get("sunrise","none")
     sunset = sys_sun.get("sunset","none")
-    sunrise_time = datetime.datetime.fromtimestamp(surise).strftime('%Y-%m-%d %H:%M:%S')
+    sunrise_time = datetime.datetime.fromtimestamp(sunrise).strftime('%Y-%m-%d %H:%M:%S')
+    sunset_time = datetime.datetime.fromtimestamp(sunset).strftime('%Y-%m-%d %H:%M:%S')
     print(sunrise_time)
+    print(sunset_time)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage("城市:{0}\n地址:{1}\n溫度: {2}\n最高溫度:{3}\n最低溫度:{4}\n相對溼度:{5}%".format(city_name, address, temp, max_temp, min_temp, humidity))
