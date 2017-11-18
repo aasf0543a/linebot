bc0d0ee4482d33f191ab15bc60f28c42e1c493e8
aasf0543a <33273330+aasf0543a@users.noreply.github.com>
2017-11-18 22:14:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -341,11 +341,11 @@ def handle_location_message(event):
     sunset = sys_sun.get("sunset","none")
     tpe = pytz.timezone('Asia/Taipei')
     sunrise_time = datetime.fromtimestamp(sunrise).strftime('%Y-%m-%d %H:%M:%S').replace(tzinfo=tpe)
-    sunset_time = datetime.datetime.fromtimestamp(sunset).strftime('%Y-%m-%d %H:%M:%S')
+    sunset_time = datetime.fromtimestamp(sunset).strftime('%Y-%m-%d %H:%M:%S').replace(tzinfo=tpe)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage("城市:{0}\n地址:{1}\n溫度: {2}\n最高溫度:{3}\n最低溫度:{4}\n相對溼度:{5}%\n日出時間:{6}\n日落時間:{7}"
-                        .format(city_name, address, temp, max_temp, min_temp, humidity, local_sunrise_time, sunset_time)))
+                        .format(city_name, address, temp, max_temp, min_temp, humidity, sunrise_time, sunset_time)))
 #將收到的訊息，定義貼圖的Event
 @handler.add(MessageEvent, message=StickerMessage)
 def handle_sticker_message(event):
