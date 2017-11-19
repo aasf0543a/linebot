51928aa115a805307ff87d28e459590f44d64759
aasf0543a <33273330+aasf0543a@users.noreply.github.com>
2017-11-19 11:30:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -344,7 +344,7 @@ def handle_location_message(event):
     sunrise_time = datetime.fromtimestamp(sunrise)#UTC時間轉成日期跟時間格式
     sunset_time = datetime.fromtimestamp(sunset)#UTC時間轉成日期跟時間格式
     now = datetime.now() #目前時間UTC+0
-    local_now = datetime.fromtimestamp(now) + timedelta(hours=8)
+    local_now = datetime.datetime(now) + timedelta(hours=8)
     print(local_now)
     local_sunrise_time = sunrise_time + timedelta(hours=8)#日出時間UTC+8
     local_sunset_time = sunset_time + timedelta(hours=8)#日落時間UTC+8
