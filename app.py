bf64ff272e07f643324b635bffa8c7db974ccf7a
aasf0543a <33273330+aasf0543a@users.noreply.github.com>
2019-02-01 18:06:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -623,6 +623,8 @@ def handle_message(event):
     else:#接收到什麼訊息，就回什麼訊息，應聲蟲
         str = event.message.text
         separate = str.split(' ', 1)
+        str1 = str(separate[0])
+        if separate == str1:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "已記錄 " + separate[1] + " 時間\n" + "地圖: 1F"))
     
 if __name__ == '__main__':app.run()# 運行本項目，host=0.0.0.0可以讓其他電腦也能訪問到該網站，port指定訪問的埠。默認的host是127.0.0.1，port為5000
