85091351b17bb650a07df0b2b81cf3abd76fb36a
aasf0543a <33273330+aasf0543a@users.noreply.github.com>
2019-10-28 16:46:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -318,7 +318,7 @@ def apple_Finance():
     return content
 
 def fuel():
-    target_url = 'http://www.taiwanoil.org'
+    target_url = 'http://www.taiwanoil.org/z.php?z=oiltw&c=94abf0&tz=Asia/Taipei&tf=1'
     print('Start parsing fuel....')
     res = requests.get(target_url)
     #print(res.status_code)
@@ -327,7 +327,7 @@ def fuel():
     #else:
     #    print('Fail')
     oilpage = res.encode('latin1', 'ignore').decode('big5')
-    return cotent(oilpage)
+    return oilpage
             
 #將收到的訊息為Location
 @handler.add(MessageEvent, message=LocationMessage)
