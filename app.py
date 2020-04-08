f2e5c53b2095193d882ace15f0f18cd962031d6a
aasf0543a <33273330+aasf0543a@users.noreply.github.com>
2020-04-08 11:38:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -630,7 +630,9 @@ def handle_message(event):
     else:#接收到什麼訊息，就回什麼訊息，應聲蟲
         str = event.message.text
         t = str.split(' ', 1)
-        stream_url = 'https://google-translate-proxy.herokuapp.com/api/tts?query='+ t +'&language=zh-tw'
+        transfer = quote(t)
+        print(transfer)
+        stream_url = 'https://google-translate-proxy.herokuapp.com/api/tts?query='+ transfer +'&language=zh-tw'
         print(stream_url)
         message = AudioSendMessage(original_content_url = stream_url ,duration=20000)
         line_bot_api.reply_message(event.reply_token, message)
