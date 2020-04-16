0228b71842a714f185f56d45ebd31492c7421e1d
aasf0543a <33273330+aasf0543a@users.noreply.github.com>
2020-04-17 00:06:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -90,17 +90,19 @@ def apple_news():
     res = rs.get(target_url, verify=False)
     soup = BeautifulSoup(res.text, 'html.parser')
     content = ""
-    for index, data in enumerate(soup.select('.aht_title a')):
-        print(data)
-        if index == 15:
-            return content
+    test = soup.select('.aht_title a')
+    print(test)
+    #for index, data in enumerate(soup.select('.aht_title a')):
+    #    print(data)
+    #    if index == 15:
+    #        return content
 #        if head in data['href']:
 #        title = data.select('title')[0].text
-        else:
-            link = data['href']
-            title = data['title']
-            content += '{}\n{}{}\n\n'.format(index, 'https://tw.appledaily.com', link)
-            return content
+    #    else:
+    #        link = data['href']
+    #        title = data['title']
+    #        content += '{}\n{}{}\n\n'.format(index, 'https://tw.appledaily.com', link)
+    #        return content
 
 
 def get_page_number(content):
