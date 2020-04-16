37d8e415106e8e879e54bd092ae955e84895c1c8
aasf0543a <33273330+aasf0543a@users.noreply.github.com>
2020-04-17 00:02:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ def apple_news():
     res = rs.get(target_url, verify=False)
     soup = BeautifulSoup(res.text, 'html.parser')
     content = ""
-    for index, data in enumerate(soup.select('div.aht_title.a')):
+    for index, data in enumerate(soup.select('.aht_title a')):
         print(data)
         if index == 15:
             return content
