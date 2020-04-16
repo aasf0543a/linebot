338d4768ff409ecb87a361bb0322fc544e52b4c4
aasf0543a <33273330+aasf0543a@users.noreply.github.com>
2020-04-16 15:56:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ def apple_news():
     soup = BeautifulSoup(res.text, 'html.parser')
     print(soup)
     content = ""
-    for index, data in enumerate(soup.select('.section a'), 0):
+    for index, data in enumerate(soup.select('aht_play'), 0):
         if index == 15:
             return content
         if head in data['href']:
